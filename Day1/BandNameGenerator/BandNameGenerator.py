7b965c625f0315a3003ea716b5695398c1bb97f4
Yilliee <yilliee@proton.me>
2023-06-05 00:59:57 +0500

    Day1 : Strings and Input/Output : Band Name Generator
     - The BandNameGenerator.py suggests a new band name
       by concatinating the Birth City and Pet name entered
       by the user.

---
+++ Day1/BandNameGenerator/BandNameGenerator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator")
+
+#2. Ask the user for the city that they grew up in.
+print("What's the name of the city you grew up in?")
+city_name = input()
+
+#3. Ask the user for the name of a pet.
+print("What's you pet's name?")
+pet_name = input()
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city_name + " " + pet_name)
