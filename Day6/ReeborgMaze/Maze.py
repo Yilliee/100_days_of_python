14da6a324b581fe5cad587d278a02af7125d6678
Yilliee <yilliee@proton.me>
2023-06-18 11:40:05 +0500

    Day6 : While Loops, Functions : Reeborg's World's Maze Challenge v2
     - Fix the character being stuck in loops if it starts in open
       spaces.

--- Day6/ReeborgMaze/Maze.py
+++ Day6/ReeborgMaze/Maze.py
@@ -3,11 +3,22 @@ def turn_right():
     turn_left()
     turn_left()
 
+turn_count = 0
 while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
+    # If we are just ran a full circle
+    # then try to move to the front or if
+    # that's not possible then turn left
+    if right_is_clear() and turn_count < 4:
+            turn_right()
+            move()
+            turn_count += 1
     elif front_is_clear():
         move()
+        turn_count = 0
     else:
         turn_left()
+        # Don't reset the turn count if we 
+        # just ran a full loop, until we are
+        # in the clear
+        if turn_count < 4:
+            turn_count = 0
