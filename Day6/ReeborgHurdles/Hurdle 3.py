52384e911d3333f99567ef66986ca8366e24f6a8
Yilliee <yilliee@proton.me>
2023-06-18 11:32:18 +0500

    Day6 : While Loops, Functions : Reeborg's World's challenges
     - Reeborg' World (https://reeborg.ca) is aimed at help
       beginners to learn programming concepts using Python
       and/or Javascript
     - The Hurdle and Maze challenge mainly focuses on:
        - Function definitions and calling functions
        - Conditionals
        - Code Reusability

---
+++ Day6/ReeborgHurdles/Hurdle 3.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
+    
+done()
